e7f3a7c9079b734c774fb4278c1f72a3629f6a2c
Pieter Joubert <pieter@directdebit.co.za>
2021-01-19 09:22:06 +0200

    move to influxdb

--- crawler/handler.py
+++ crawler/handler.py
@@ -1,40 +1,31 @@
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "mv8uWtXd4-5_3pCKuoHRy5jV52PNqMDLkflteA3SAFdkf9q5Q1z1NtRQ7XwyWWmxzg3IrHMGkfWtolpxs4RiGw=="
+org = "ranktank"
+bucket = "serp_rank"
+
+client = InfluxDBClient(url="http://localhost:8086", token=token)
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+
 def save_rankings(rank_data, category_definitions, latest_ranks):
-    # file2write = open("thedata.txt", 'w')
-    # for item in rank_data['ranks']['US']:
-    #     print(item)
-    #     if item.get('category') is None:
-    #         item['category'] = 0
-    #     item['category_name'] = category_definitions[item['category']]
-    #     rank = Rank(date=date.fromisoformat(item["date"]), rank=item["rank"], db=item["db"],
-    #                 category=category if (category := item.get("category")) else 0)
-    #     session.add(rank)
-    #     file2write.write(str({
-    #         "date": item["date"],
-    #         "rank": item["rank"], "db": item["db"],
-    #         "category": category if (category := item.get("category")) else 0}))
-    # file2write.close()
-    file2write = open("/home/app/mount/metrics", 'w')
+    file2write = open("metrics", 'w')
+    sequence = []
+    time = datetime.utcnow()
     for item in latest_ranks:
         print(item)
-        # latest_rank = LatestRank(
-        #     rank=item["rank"],
-        #     category=item["category"],
-        #     category_name=item["category_name"],
-        #     date=date.today()
-        # )
-        # session.add(latest_rank)
-
-        # file2write.write(str({
-        #     # "date": date.today(),
-        #     "rank": item["rank"],
-        #     # "db": item["db"],
-        #     "category": item["category"],
-        #     "category_name": item["category_name"]
-        # }))
-
+        print(f'mem,host=semrush category_name="{item["category_name"]}" rank={item["rank"]}')
         file2write.write(
             f'serp_rank{{category="{item["category"]}",category_name="{item["category_name"]}"}} {item["rank"]} \n'
         )
-
-        # session.commit()
+        sequence.append(f'mem,host=host1 category={item["category"]} rank={item["rank"]}')
+        point = Point("mem") \
+            .tag("category_name", f'{item["category_name"]}') \
+            .field("rank", float(item["rank"])+1)\
+            .time(time, WritePrecision.NS)
+        write_api.write(bucket, org, point)
     file2write.close()
