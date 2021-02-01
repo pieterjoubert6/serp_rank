123ec027f8a21d5b2ebe5545b964bf1b3d4e1829
Pieter Joubert <pieter@directdebit.co.za>
2021-02-01 12:25:29 +0200

    Change influxdb data format

--- crawler/handler.py
+++ crawler/handler.py
@@ -3,21 +3,33 @@ from datetime import datetime
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-from conf import API_TOKEN, BUCKET, ORG
+from conf import API_TOKEN, BUCKET, ORG, REGION_URLS
 
 client = InfluxDBClient(url="http://localhost:8086", token=API_TOKEN, org=ORG)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-def save_rankings(latest_ranks):
+def save_rankings(latest_ranks, current_url):
+    REGION_URLS
     sequence = []
+    region = None
+    device = None
+    for item in REGION_URLS:
+        if item['url'] == current_url:
+            region = item['region']
+            device = item['device']
+            break
     time = datetime.utcnow()
     for item in latest_ranks:
         sequence.append({
-            "measurement": item["category_name"],
+            "measurement": region,
             "time": time,
+            "tags": {
+                "category_name": item["category_name"],
+                "device": device
+            },
             "fields": {
-                "rank": float(item["rank"])
+                "value": float(item["rank"])
             }
         })
     write_api.write(BUCKET, ORG, sequence)
